e1e384f181e9a151977f5f33e15fdc863e2e2c77
Ayush8481Lab <wonder945177@gmail.com>
2026-09-24 00:27:13 +0530

    Update fetch method to POST in dish.js
    Changed the HTTP method from GET to POST for the fetch request.

--- api/dish.js
+++ api/dish.js
@@ -13,13 +13,13 @@ export default async function handler(req, res) {
       'https://apiv2.sonyliv.com/AGL/5.0/A/ENG/MWEB/IN/UP/CONTENT/VIDEOURL/VOD/1090476406';
 
     const upstream = await fetch(targetUrl, {
-      method: 'GET',
+      method: 'POST',
       headers: {
         'accept': 'application/json, text/plain, */*',
         'user-agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
         "cookie": "sl_device_token=3a4f92a0919d4568931f657896b51b5a-1790188990243; sl_ppid=3a4f92a0919d4568931f657896b51b5a; ak_cf=g8f0-ju0v-w04o-3rxa",
-        'tenant-code': 'dishtv',
+        
         'referer': 'https://sonyliv.com/',
         'origin': 'https://sonyliv.com,
       },
